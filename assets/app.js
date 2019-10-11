d156401cb61eab3c90fe679885857b70898249ee
Cohen <dcoco91@gmail.com>
2019-10-11 15:53:21 -0400

    more value grabbing, differentiating between operators and numbers

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
     
+
     const $button = $(".button");
+    let type = "";
     $button.on("click", function(){
-        buttonPress($(this).children().text())
+        
+        if($(this).hasClass("num")){
+            type = "num"
+        } else if($(this).hasClass("op")){
+            type = "op"
+        } else {
+            type = "sign"
+        }
+        
+        buttonPress($(this).children().text(), type)
     });
     
-    function buttonPress(value){
-        console.log(value)
+    function buttonPress(value, type){
+        console.log(type);
+        // switch(value){
+        //     case 
+        // }
     }
 
 });
