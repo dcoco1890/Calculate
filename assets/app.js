0c7d35a9ef87149d0393a18a0908a6d234db999b
Cohen <dcoco91@gmail.com>
2019-10-11 16:04:23 -0400

    numbers are concat'd together in the answer spot

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
     
-
+    // declaring things
     const $button = $(".button");
+    const $answer = $("#numAnswer");
     let type = "";
+    let numArr = [];
+    let opPress = false;
+
+    //button click method
     $button.on("click", function(){
-        
+
+        // $answer.children().text()
         if($(this).hasClass("num")){
             type = "num"
         } else if($(this).hasClass("op")){
@@ -12,15 +18,21 @@ $(document).ready(function () {
         } else {
             type = "sign"
         }
-        
+        // sending the value of the button, and what kind it is, to this 
+        // button press function
         buttonPress($(this).children().text(), type)
     });
     
     function buttonPress(value, type){
-        console.log(type);
-        // switch(value){
-        //     case 
-        // }
+        console.log(type, value); // just for me
+        // console.log($answer.children().text())
+        let num = $answer.children().text();
+
+        if(type === "num" && !opPress){
+            num += value;
+            $answer.children().text(num)
+        }
+
     }
 
 });
