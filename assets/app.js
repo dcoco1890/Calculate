70b9e42e17df985b88d779ffccb7b5bac6feeaa6
Cohen <dcoco91@gmail.com>
2019-10-14 13:58:00 -0400

    broken right now, but changing messy function into more managable parts

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
-    
+
     // declaring things
     const $button = $(".button");
     const $answer = $("#main");
     const $second = $("#other");
-    let type = "";
+    let sign = "";
     let numArr = [];
     let opArr = [];
     let opPress = false;
     let num = "";
 
     //button click method
-    $button.on("click", function(){
+    $button.on("click", function () {
 
         // $answer.children().text()
-        if($(this).hasClass("num")){
+        if ($(this).hasClass("num")) {
+
             numPress($(this).data("num"))
-        } else if($(this).hasClass("op")){
+
+        } else if ($(this).hasClass("op")) {
+
+            $(this).data("op") !== "="
+
             type = "op"
         } else {
             type = "sign"
@@ -25,37 +30,37 @@ $(document).ready(function () {
         // button press function
         buttonPress($(this).children().text(), type)
     });
-    
-    function buttonPress(value, type){
+
+    function buttonPress(value, type) {
         console.log(type, value); // just for me
-        
 
-        
-        
 
-        if(opPress){
+
+
+
+        if (opPress) {
             $answer.text("");
             $second.text(num)
             opPress = false;
             num = "";
         }
 
-       
-        if(type === "num" ){
 
-          
-        // if the type is operator, push num into array
-        } else if(type === "op"){
+        if (type === "num") {
+
+
+            // if the type is operator, push num into array
+        } else if (type === "op") {
 
             opPress = true;
-            if(num !== ""){
+            if (num !== "") {
                 numArr.push(Number(num));
             }
             console.log(numArr);
-            
-        } else{
+
+        } else {
             //  clear all values
-            if(value === "AC"){
+            if (value === "AC") {
                 numArr = [];
                 $answer.text("");
                 $second.text("");
@@ -64,7 +69,7 @@ $(document).ready(function () {
 
     }
     // for when a regular number gets pressed. concat nums together
-    function numPress (val) {
+    function numPress(val) {
         console.log(val);
         num = $answer.text();
         num += val;
