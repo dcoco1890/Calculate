7b673aef54e1884884b8571649ab3fd27189cc89
Cohen <dcoco91@gmail.com>
2019-10-25 13:02:49 -0400

    added code to the number pressing function that limits input to numbers smaller than 8 digits long

--- assets/app.js
+++ assets/app.js
@@ -1,119 +1,109 @@
-$(document).ready(function () {
+$ (document).ready (function () {
+  // declaring things
+  const $button = $ ('.button');
+  const $answer = $ ('#main');
+  const $second = $ ('#otherNums');
+  let evalStr = '';
+  let numArr = [];
+  let opArr = [];
+  let keyedOpButton = false;
+  let num = '';
+  let valid = $ ('#validation');
 
-    // declaring things
-    const $button = $(".button");
-    const $answer = $("#main");
-    const $second = $("#otherNums");
-    let evalStr = "";
-    let numArr = [];
-    let opArr = [];
-    let keyedOpButton = false;
-    let num = "";
-
-
-    // makes the buttons a lil' bigger when you hover
-    $button.mouseover(function () {
-        $(this).toggleClass("button-click")
-    }).mouseout(function () {
-        $(this).toggleClass("button-click")
+  // makes the buttons a lil' bigger when you hover
+  $button
+    .mouseover (function () {
+      $ (this).toggleClass ('button-click');
     })
-
-    //button click method
-    $button.on("click", function () {
-
-
-        // $answer.children().text()
-        if ($(this).hasClass("num")) {
-
-            numPress($(this).data("num"))
-
-        } else if ($(this).hasClass("op")) {
-
-            opPress($(this).data("op"))
-
-
-        } else if ($(this).data("clear") === "yes") {
-
-            $answer.text("");
-            $second.text("");
-            evalStr = "";
-
-        } else if ($(this).data("clears") === "equals") {
-            evaluate();
-        }
-
-
+    .mouseout (function () {
+      $ (this).toggleClass ('button-click');
     });
 
-    // function buttonPress(value, type) {
-    //     console.log(type, value); // just for me
+  //button click method
+  $button.on ('click', function () {
+    // $answer.children().text()
+    if ($ (this).hasClass ('num')) {
+      numPress ($ (this).data ('num'));
+    } else if ($ (this).hasClass ('op')) {
+      opPress ($ (this).data ('op'));
+    } else if ($ (this).data ('clear') === 'yes') {
+      $answer.text ('');
+      $second.text ('');
+      evalStr = '';
+    } else if ($ (this).data ('clears') === 'equals') {
+      evaluate ();
+    }
+  });
 
+  // function buttonPress(value, type) {
+  //     console.log(type, value); // just for me
 
-    //     if (opPress) {
-    //         $answer.text("");
-    //         $second.text(num)
-    //         opPress = false;
-    //         num = "";
-    //     }
+  //     if (opPress) {
+  //         $answer.text("");
+  //         $second.text(num)
+  //         opPress = false;
+  //         num = "";
+  //     }
 
-    //     if (type === "num") {
+  //     if (type === "num") {
 
+  //         // if the type is operator, push num into array
+  //     } else if (type === "op") {
 
-    //         // if the type is operator, push num into array
-    //     } else if (type === "op") {
+  //         opPress = true;
+  //         if (num !== "") {
+  //             numArr.push(Number(num));
+  //         }
+  //         console.log(numArr);
 
-    //         opPress = true;
-    //         if (num !== "") {
-    //             numArr.push(Number(num));
-    //         }
-    //         console.log(numArr);
+  //     } else {
+  //         //  clear all values
+  //         if (value === "AC") {
+  //             numArr = [];
+  //             $answer.text("");
+  //             $second.text("");
+  //         }
+  //     }
+  // }
 
-    //     } else {
-    //         //  clear all values
-    //         if (value === "AC") {
-    //             numArr = [];
-    //             $answer.text("");
-    //             $second.text("");
-    //         }
-    //     }
-    // }
-
-    // for when a regular number gets pressed. concat nums together
-    function numPress(val) {
-
-        if (keyedOpButton) {
-            $answer.text("");
-            keyedOpButton = false;
-        }
-        console.log(val);
-        num = $answer.text();
-        num += val;
-        $answer.text(num)
+  // for when a regular number gets pressed. concat nums together
+  function numPress (val) {
+    // if you've pressed the operator, clear the "main" number
+    if (keyedOpButton) {
+      $answer.text ('');
+      keyedOpButton = false;
     }
 
-    // operator press function
-    function opPress(val) {
+    let x = $answer.text ().length;
 
-        keyedOpButton = true;
-        // create new element, add num txt to it
-        let newp = $("<div class='o-nums'>")
-        newp.text(num)
-
-        // add current num to top div
-        $second.append(newp);
-
-
-        evalStr += num + ' ' + val; //only will do 2 numbers, i think
-        console.log(evalStr);
+    if (x > 6) {
+      valid.text ("Number too big. Why don't you try doing something with it?");
+    } else {
+      num = $answer.text ();
+      num += val;
+      $answer.text (num);
     }
+  }
 
-    function evaluate() {
+  // operator press function
+  function opPress (val) {
+    keyedOpButton = true;
+    // create new element, add num txt to it
+    let otherNum = $ ("<div class='o-nums'>");
+    otherNum.text (num);
 
-        // same as opPress, DRY later
-        let newp = $("<div class='o-nums'>")
-        newp.text(num)
-        $second.append(newp);
-        console.log(eval(evalStr));
-    }
+    // add current num to top div
+    $second.append (otherNum);
 
+    evalStr += num + ' ' + val; //only will do 2 numbers, i think
+    console.log (evalStr);
+  }
+
+  function evaluate () {
+    // same as opPress, DRY later
+    let newp = $ ("<div class='o-nums'>");
+    newp.text (num);
+    $second.append (newp);
+    console.log (eval (evalStr));
+  }
 });
